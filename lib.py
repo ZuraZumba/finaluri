7aee8c2212f03622d3f00d71b46afc0ff49f925b
Ming Chen <ming.chen@triflesoft.org>
2023-07-17 01:08:34 +0400

    pytest_absent_black_passed

--- lib.py
+++ lib.py
@@ -1,4 +1,4 @@
-# 30478168954171413163
+# 21137168954171428428
 class QuadraticEquationSolver:
     def __init__(self, a, b, c):
         self.a = a
@@ -8,7 +8,7 @@ class QuadraticEquationSolver:
     def solve(self):
         d = self.b**2 - 4 * self.a * self.c
 
-        if d < 0:
+        if d <= 0:
             return []
 
         if d == 0:
